7263549d3c6e623b4ee4ecfdea857d41b3498ba4
Justin Daining <justin@justindaining.com>
2017-03-14 12:34:16 -0600

    through page 107

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -1,7 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  songCreationStarted: false,
+  title: '',
+
+  isAddButtonDisabled: Ember.computed('title', function() {
+    return Ember.isEmpty(this.get('title'));
+  }),
+
+  canCreateSong: Ember.computed('songCreationStarted', 'model.songs.length', function() {
+    return this.get('songCreationStarted') || this.get('model.songs.length');
+  }),
+
   actions: {
+    enableSongCreation() {
+      this.set('songCreationStarted', true);
+    },
     updateRating(params) {
       var song = params.item,
           rating = params.rating;
